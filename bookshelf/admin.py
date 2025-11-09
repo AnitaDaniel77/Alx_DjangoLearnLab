89ab58e340c298e354c96c5d9a245123c4a8dcde
Anita Daniel <anitadaniel1977@gmail.com>
2025-11-09 22:27:28 +0200

    Complete Django admin integration and CRUD documentation

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Book
 
 @admin.register(Book)
@@ -9,3 +6,4 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+
