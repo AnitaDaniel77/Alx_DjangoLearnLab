743ad797031af9ac48b974faa7156363462d4704
Anita Daniel <anitadaniel1977@gmail.com>
2025-12-13 16:29:07 +0200

    Fix import: use DjangoFilterBackend for filtering

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics, permissions, filters
+from rest_framework import generics,permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
